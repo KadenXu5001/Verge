af583ca097542a3268219c2b38a87f10ae1584d4
kadenxu5001 <catty5001@gmail.com>
2026-09-19 13:48:06 -0400

    Add QNX root and tmp filesystem files

---
+++ qnx/filesystem/root/udp_listener.py
@@ -0,0 +1,9 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("0.0.0.0", 4210))
+print("listening...")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(f"{addr}: {data.decode()}")
